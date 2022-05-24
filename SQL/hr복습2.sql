7073e439ffbef232c0d36515a3a1cacbb649869a
ehdghks316 <41100768+ehdghks316@users.noreply.github.com>
2022-05-24 14:12:26 +0900

    Delete hr복습2.sql

--- SQL/hr복습2.sql
+++
@@ -1,8 +0,0 @@
-SELECT employee_id || ' ' || last_name || ' ' || first_name
-FROM employees;
-
-SELECT department_name || q'(Depatment's Manager ID:)' || manager_id "goodD"
-FROM departments;
-
-SELECT last_name || ', ' ||job_id "Employee and Title"
-FROM employees;
